2fe4beea4ce25123215ab3b120836dac4e4a3fe5
nmahmud79 <nurul.mahmud@gmail.com>
2019-10-20 15:23:46 -0400

    MY FIRST UPLOAD

---
+++ src/DateTime.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DateTime {
+
+	public static void main(String[] args) {
+		
+		LocalTime myObj = LocalTime.now();
+		
+		System.out.println(myObj);
+
+	}
+
+}
